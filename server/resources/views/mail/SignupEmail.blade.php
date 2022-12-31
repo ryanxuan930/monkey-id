d6793c69400c522e01f3842f273ed733d5b9d59a
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2022-12-31 17:40:44 +0800

    Update SignupEmail.blade.php

--- server/resources/views/mail/SignupEmail.blade.php
+++ server/resources/views/mail/SignupEmail.blade.php
@@ -1,4 +1,28 @@
-<div>
-    親愛的 {{$variables['name']}}：<br>
-    感謝您註冊MonkeyID。
+<div style="width: 100%; box-sizing: border-box; padding: 3%; font-size: 11pt; color: #424242; font-family: sans-serif;">
+    <div style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
+        <div style="background-color: rgb(255, 247, 237, 1); padding: 5px 10px;">
+            <img style="display: inline-block; height: 60px;" src="https://ryanxuan930.github.io/cdn/nsysu_athletics/monkeyid/monkey_id.svg">
+        </div>
+        <div>
+            <div style="padding: 20px;">
+                親愛的 {{$variables['name']}}：<br>
+                <br>
+                您已成功註冊MonkeyID<br>
+                帳號為：{{$variables['account']}}<br>
+                <br>
+                順頌時祺<br>
+                MonkeyID<br>
+                <br>
+                <div style="font-size: 9pt; color:#BDBDBD;">此為系統自動發送之信件，請勿回覆。</div>
+            </div>
+        <div style="background-color: rgba(255, 247, 237, 1); padding: 10px; text-align: center">
+            <!--
+            <div style="font-size: 10pt">
+                <a style="text-decoration: none; color: #424242; display: inline-block; margin: 5px 10px;" href="https://nsysugo.nsysu.edu.tw/">官方網站</a>
+                <a style="text-decoration: none; color: #424242; display: inline-block; margin: 5px 10px;" href="https://nsysugo.nsysu.edu.tw/shopping">選購商品</a>
+                <span style="color: #424242 display: inline-block; margin: 5px 10px;">(07)525-2000 #6687、#6688、#6689</span>
+            </div>-->
+            <div style="font-size: 6pt">Copyright © 2023 MonkeyID by NSYSU Athletics</div>
+        </div>
+    </div>
 </div>
