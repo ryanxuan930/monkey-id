50ccdd7e134d7ffa7d0cfeaa89d70827e83bb13e
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-01-02 17:09:33 +0800

    Update UserController.php

--- server/app/Http/Controllers/Auth/UserController.php
+++ server/app/Http/Controllers/Auth/UserController.php
@@ -327,14 +327,12 @@ class UserController extends Controller
     // file handler
     function upload(Request $request)
     {
-        if ($request->hasFile('image')) {
-            $user = auth()->user();
-            if ($user->file) {
-                Storage::disk('public')->delete($user->file);
-            } 
-            $path = $request->file('image')->store('images', 'public');
-            User::where('u_id', $user->u_id)->update(['file' => $path]);
-        }
-        return response()->json(['status'=>$request]);
+        $user = auth()->user();
+        if ($user->file) {
+            Storage::disk('public')->delete($user->file);
+        } 
+        $path = $request->file('image')->store('images', 'public');
+        User::where('u_id', $user->u_id)->update(['file' => $path]);
+        return response()->json(['status'=>$path]);
     }
 }
