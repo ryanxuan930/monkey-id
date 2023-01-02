69df553b5dff522fe00bdf91d1cf983e37549ad8
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-01-02 15:36:28 +0800

    Update UserController.php

--- server/app/Http/Controllers/Auth/UserController.php
+++ server/app/Http/Controllers/Auth/UserController.php
@@ -191,7 +191,7 @@ class UserController extends Controller
         $email = $temp['email'];
         $time = new DateTime();
         EmailLog::whereDate('valid_until', '>=', $time)->whereTime('valid_until', '>=', $time)->update(['used' => 1]);
-        return response()->json(['status' => 'U01', 'message' => EmailLog::whereDate('valid_until', '<', $time)->whereTime('valid_until', '<', $time)->count()], 200);
+        return response()->json(['status' => $time, 'message' => EmailLog::whereDate('valid_until', '<', $time)->whereTime('valid_until', '<', $time)->count()], 200);
         $data = explode('@', $email);
         $domain = array_pop($data);
         if (!str_contains($domain, 'edu')) {
